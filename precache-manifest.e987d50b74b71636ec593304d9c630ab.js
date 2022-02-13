393bb65e3ead6cf13573335e347d15cbb7e0f49b
Mehmet Kırmızıkaya <mehmetkirmizikaya46@gmail.com>
2022-02-13 23:13:29 +0300

    Updates

---
+++ precache-manifest.e987d50b74b71636ec593304d9c630ab.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4585b747283381268c75b483b746244",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6aedfe7851dfb519d9b",
+    "url": "/static/css/2.a279b891.chunk.css"
+  },
+  {
+    "revision": "d1dcdb5ec88f292c0750",
+    "url": "/static/css/main.16a017df.chunk.css"
+  },
+  {
+    "revision": "f6aedfe7851dfb519d9b",
+    "url": "/static/js/2.02c96ba0.chunk.js"
+  },
+  {
+    "revision": "522e03d803bfdb0e0cda4e24f58dcf37",
+    "url": "/static/js/2.02c96ba0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "258b6f5280668ee30e60",
+    "url": "/static/js/3.958ba28d.chunk.js"
+  },
+  {
+    "revision": "8076a39f2c6eb4b606ff15bd0342709c",
+    "url": "/static/js/3.958ba28d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1dcdb5ec88f292c0750",
+    "url": "/static/js/main.c76f1b26.chunk.js"
+  },
+  {
+    "revision": "b9839693ae1f373f70b3",
+    "url": "/static/js/runtime-main.0cde675a.js"
+  },
+  {
+    "revision": "73e6b012bf1e5ab3ba41033aa8f977de",
+    "url": "/static/media/flags_responsive.73e6b012.png"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/static/media/primeicons.e01fd413.eot"
+  }
+]);
